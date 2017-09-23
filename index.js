6e1b1f33a91bae4f9c0f015d1065b2154cc85f6d
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-23 06:34:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function breakOut(array, changeValue, stopValue) {
+  for (i=0; i < array.length; i++) {
+    if (array[i] != stopValue) {
+        array[i] = changeValue
+    }
+    else {
+      break
+    }
+    }
+  }
+}
